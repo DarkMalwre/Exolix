e155196e4cf41e7b07c59e29bb0b45c525190463
XFaon <85759101+XFaonAE@users.noreply.github.com>
2021-10-15 19:13:00 -0400

    Chore: Updated events

--- Exolix/Sockets/Server/ServerConnection.cs
+++ Exolix/Sockets/Server/ServerConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebSocketSharp;
 using WebSocketSharp.Server;
 
 namespace Exolix.Sockets.Server
@@ -20,14 +21,24 @@ namespace Exolix.Sockets.Server
     public class CoreServerConnection: WebSocketBehavior
     {
         public SocketServer? Server;
+        public ServerConnection? Connection;
 
         protected override void OnOpen()
         {
             ServerConnection connection = new ServerConnection(this);
+            Connection = connection;
 
             foreach (var eventAction in Server!.OnOpenEvents.ToArray())
             {
                 eventAction(connection);
+            }
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            foreach (var eventAction in Server!.OnMessageEvents.ToArray())
+            {
+                eventAction();
             }
         }
     }
