cb8138a9de55c6a49ca15bb9160d181dd45acf6f
XfaonAE <xfaonae@gmail.com>
2021-10-24 10:18:33 -0400

    Updt

---
+++ Exolix/ApiHost/ApiHost.cs
@@ -0,0 +1,58 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exolix.ApiHost
+{
+    public class ApiHostCertificate {
+        public string Certificate = "";
+        public string Key = "";
+    }
+
+    public class ApiHostSettings
+    {
+        public string Host = "0.0.0.0";
+        public int? Port = null;
+        public ApiHostCertificate? Certificate = null;
+    }
+
+    public class ApiHost
+    {
+        /// <summary>
+        /// Settings for the API endpoint server
+        /// </summary>
+        private ApiHostSettings Settings;
+
+        /// <summary>
+        /// Create a new API endpoint server
+        /// </summary>
+        /// <param name="settings">Settings for the server</param>
+        public ApiHost(ApiHostSettings? settings)
+        {
+            if (settings == null)
+            {
+                Settings = new ApiHostSettings();
+                return;
+            }
+
+            Settings = settings;
+        }
+
+        /// <summary>
+        /// Start listening for API commands
+        /// </summary>
+        public void Run()
+        {
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("Open!");
+                socket.OnClose = () => Console.WriteLine("Close!");
+                socket.OnMessage = message => socket.Send(message);
+            });
+        }
+    }
+}
--- ExolixTests/UnitTest1.cs
+++ ExolixTests/UnitTest1.cs
@@ -1,120 +1,19 @@
-﻿using Exolix.Json;
-using Exolix.Sockets.Client;
-using Exolix.Sockets.Server;
+﻿using Exolix.ApiHost;
 using Exolix.Terminal;
-using Pastel;
+using Fleck;
 using System;
-using System.Net;
-using System.Net.Sockets;
-using WebSocketSharp.Server;
 
-namespace ExolixTests
+public class Tests
 {
-    public class MessageType
+    public static void Main(string[] args)
     {
-        public string? Msg = "No Message Provided";
-    }
+        Logger.Info("Starting server");
 
-    public class Tests
-    {
-        public class DemoMessage
+        var api = new ApiHost(new ApiHostSettings
         {
-            public string? Message;
-        }
 
-        public static void Main(string[] args)
-        {
-            Logger.Info("Starting API server");
-            SocketServer server = new SocketServer(new SocketServerSettings
-            {
-                Port = 80
-            });
+        });
 
-            server.OnOpen((connection) =>
-            {
-                Logger.Info(" + New Connection");
-
-                connection.OnMessage("main", (msg) =>
-                {
-                    MessageType message = JsonHandler.Parse<MessageType>(msg);
-                    Logger.Info(" Message: " + message.Msg);
-                    connection.Send("main", new MessageType
-                    {
-                        Msg = message.Msg,
-                    });
-                });
-
-                connection.OnClose(() =>
-                {
-                    Logger.Info("- Connection Closed");
-                });
-
-                connection.Send("main", new MessageType
-                {
-                    Msg = "Heyy"
-                });
-            });
-
-            server.Run();
-
-            Logger.Success("Server started at port 80");
-
-            //System.Threading.Thread.Sleep(500);
-
-            Logger.Info("Connecting to server via client");
-
-            SocketClient client = new SocketClient(new SocketClientSettings
-            {
-                Port = 80
-            });
-
-            client.OnOpen(() =>
-            {
-                Logger.Info("[ Client ] Connected to server");
-                client.Send("main", new MessageType
-                {
-                    Msg = "Hello new service"
-                });
-            });
-
-            client.OnMessage("main", (msg) =>
-            {
-                MessageType message = JsonHandler.Parse<MessageType>(msg);
-                Logger.Info("[ Client ] Message: " + message.Msg);
-            });
-
-            client.Run();
-
-            Logger.Info("Done with actions");
-        }
-
-        public static void DoException(SocketServer server)
-        {
-            DoExceptionSec(server);
-        }
-
-        public static void DoExceptionSec(SocketServer server)
-        {
-            try
-            {
-                server.Run();
-            }
-            catch (Exception ex)
-            {
-                Logger.ErrorException(ex);
-            }
-        }
-    }
-
-    public class HandleService: WebSocketBehavior {
-        protected override void OnMessage(WebSocketSharp.MessageEventArgs e)
-        {
-            Logger.PrintDynamic("MSG: " + e.Data);
-        }
-
-        protected override void OnOpen()
-        {
-            Logger.PrintDynamic("New Conn: " + Context.QueryString["name"]);
-        }
+        Logger.KeepAlive(true);
     }
 }
